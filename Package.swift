f9a1fec60723da33cdb797fa8a997e635cadc51c
Tomoaki Yagishita <tyagishi@gmail.com>
2024-06-18 15:47:43 +0900

    R!! adapt for SwiftLint 0.55

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         //.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.54.0"),
+        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.55.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -28,7 +28,7 @@ let package = Package(
             name: "SDSDataStructure",
             dependencies: [],
             plugins: [
-                .plugin(name: "SwiftLintPlugin", package: "SwiftLint")
+                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint")
             ]
         ),
         .testTarget(

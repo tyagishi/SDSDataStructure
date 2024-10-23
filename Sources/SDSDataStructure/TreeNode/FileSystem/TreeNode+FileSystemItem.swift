8a0d1c7a8d9e69638f2ab1e3a346eed8d2afa79c
Tomoaki Yagishita <tyagishi@gmail.com>
2024-10-23 17:23:21 +0900

    F - add nodeFromPath/pathFromRoot/fullPathToFile

---
+++ Sources/SDSDataStructure/TreeNode/FileSystem/TreeNode+FileSystemItem.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//
+//  Created by : Tomoaki Yagishita on 2024/10/23
+//  © 2024  SmallDeskSoftware
+//
+
+import Foundation
+
+extension TreeNode where T == FileSystemItem {
+    public func nodeFrom(path: any StringProtocol) -> TreeNode<FileSystemItem>? {
+        var pathComponents = path.split(separator: "/")
+        var node: TreeNode<FileSystemItem> = self.rootNode()
+        while let component = pathComponents.first {
+            guard let childNode = node.children.filter({$0.filename == component}).first else { return nil }
+            node = childNode
+            pathComponents.removeFirst()
+        }
+        return node
+    }
+
+    public func pathFromRoot() -> String {
+        var pathString = ""
+        var node = self
+        while let parent = node.parent {
+            if parent.parent == nil { break }
+            pathString = parent.filename + "/" + pathString
+            node = parent
+        }
+        return "/" + pathString
+    }
+    
+    public func fullPathToFile() -> String {
+        return self.pathFromRoot() + self.filename
+    }
+}
---
+++ Tests/SDSDataStructureTests/TreeNode/FileSystem/TreeNode_FileSystemItem_PathHandlingTests.swift
@@ -0,0 +1,49 @@
+//
+//  TreeNode_FileSystemItem_PathHandlingTests.swift
+//
+//  Created by : Tomoaki Yagishita on 2024/10/23
+//  © 2024  SmallDeskSoftware
+//
+
+import XCTest
+@testable import SDSDataStructure
+
+final class TreeNode_FileSystemItem_PathHandlingTests: XCTestCase {
+    func prepRootNode() -> TreeNode<FileSystemItem> {
+        let root = TreeNode(preferredFilename: "root", FileWrapper(directoryWithFileWrappers: [:]))
+        let intermid1 = root.addDirectory(dirName: "intermid1")
+        let intermid2 = root.addDirectory(dirName: "intermid2")
+        let _ = intermid1.addTextFile(fileName: "Leaf11.txt", text: "Hello Leaf11")
+        let _ = intermid1.addTextFile(fileName: "Leaf12.txt", text: "World Leaf12")
+        let _ = intermid2.addTextFile(fileName: "Leaf21.txt", text: "World Leaf21")
+        return root
+    }
+    
+    func test_pathFromRoot_fullPathToFile() async throws {
+        let sut = prepRootNode()
+
+        XCTAssertEqual(sut.pathFromRoot(), "/")
+        XCTAssertEqual(sut.fullPathToFile(), "/root")
+
+        let intermid1 = try XCTUnwrap(sut.search(match: { $0.filename == "intermid1" }))
+        XCTAssertEqual(intermid1.pathFromRoot(), "/")
+        XCTAssertEqual(intermid1.fullPathToFile(), "/intermid1")
+
+        let leaf12 = try XCTUnwrap(sut.search(match: { $0.filename == "Leaf12.txt" }))
+        XCTAssertEqual(leaf12.pathFromRoot(), "/intermid1/")
+        XCTAssertEqual(leaf12.fullPathToFile(), "/intermid1/Leaf12.txt")
+
+    }
+    
+    func test_nodeFromPath() async throws {
+        let sut = prepRootNode()
+
+        let intermid1 = try XCTUnwrap(sut.nodeFrom(path: "/intermid1"))
+        XCTAssertEqual(intermid1.filename, "intermid1")
+
+        let leaf21 = try XCTUnwrap(sut.nodeFrom(path: "/intermid2/Leaf21.txt"))
+        XCTAssertEqual(leaf21.filename, "Leaf21.txt")
+    }
+    
+
+}
